deab67452093224686e2e61e575dc15c55550275
Martin Eneqvist <marlun78@hotmail.com>
2020-02-18 22:23:42 +0100

    prettier

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,16 +8,16 @@ app.use((request, response, next) => {
   console.log(request.url, request.headers);
   next();
 });
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "static/index.html"))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'static/index.html')),
 );
 app.use(
-  express.static(path.join(__dirname, "static"), {
+  express.static(path.join(__dirname, 'static'), {
     setHeaders: (res, path, stat) => {
-      res.set("Cache-Control", "max-age=0, must-revalidate");
-      res.set("Expires", "-1");
-    }
-  })
+      res.set('Cache-Control', 'max-age=0, must-revalidate');
+      res.set('Expires', '-1');
+    },
+  }),
 );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
